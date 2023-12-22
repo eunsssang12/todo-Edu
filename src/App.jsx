81979fe9fe02d99423bb80d4c0efa0c7de68bad2
doca41531 <doca41531@gmail.com>
2023-12-23 00:05:25 +0900

    최종본

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,81 @@
-import { useReducer, useRef, useState } from 'react'
+import { useReducer, useState } from 'react';
+
+// useRef 란 저장공간 또는 DOM요소에 접근하기 위해 사용되는 React Hook을 뜻한다
+// useReducer 이란 useState와 약간 비슷한 hook이다 더 많은 기능이 있고 더 복잡한 state에 특히 유용하다.
 
 
-function App() {
-  const [inputs, setInputs] = useState({
-    contant : String,
-    check : Boolean,
-  })
-  const { contant, check } = inputs;
-
-const onChange = e => {
-  settodo(e.target.value)
-  const { name, value } = e.target;    
-  setInputs({      
-    ...inputs,      
-    [name]: value    
-  });  
-};
-
-const reducer = (state, action) => {
+// Reducer 함수: 투두 상태를 관리하는 함수
+const todoReducer = (state, action) => {
   switch (action.type) {
-    case "CHANGE_NAME":
-      return {
-        ...state,
-        name: action.name
-      };
+    case 'ADD_TODO':
+      return [...state, { id: Date.now(), text: action.payload }];
+    case 'DELETE_TODO':
+      return state.filter(todo => todo.id !== action.payload);
     default:
       return state;
   }
 };
 
-const [users, setUsers] = useState([
-  {
-    id : Number,
-    contant: String,
-    check: Boolean,
-  },
-]);
+// Todo 입력 폼을 담당하는 컴포넌트
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('');
 
-
-  const [todo, settodo] = useState("");
-  const [state, dispatch] = useReducer(reducer, users);
-
-  const nextId = useRef(2);  
-  const onCreate = (e) => {
+  // 폼 제출 시 실행되는 함수
+  const handleSubmit = e => {
     e.preventDefault();
-
-    const users = {
-      id: nextId.current,
-      contant,
-      check,
-    }    
-    setUsers(users.concat(users));
-    setInputs({      
-      contant: '',      
-      check: '',      
-    });
-    nextId.current += 1;  
-
-    dispatch({
-      type: "add_todo",
-      name: "이지은"
-    });
+    addTodo(text);
+    setText('');
   };
 
   return (
-      <div>
-        <form onSubmit={onCreate}>
-          <input type='text' placeholder='할일을 입력해주세요' onChange={onChange} value={todo}></input>
-          <button type='submit'>제출</button>
-          <ul>
-            {state.map((item) =><li key={item.id}>{item.contant}</li>)}
-          </ul>
-        </form>
-      </div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
 
-export default App
+// Todo 목록을 표시하고 삭제 기능을 가진 컴포넌트
+const TodoList = ({ todos, deleteTodo }) => {
+  return (
+    <ul>
+      {todos.map(todo => (
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+// 전체 애플리케이션을 관리하는 부모 컴포넌트
+const App = () => {
+  // useReducer를 사용하여 상태와 액션을 관리
+  const [todos, dispatch] = useReducer(todoReducer, []);
+
+  // 새로운 투두 추가 액션을 호출하는 함수
+  const addTodo = text => {
+    dispatch({ type: 'ADD_TODO', payload: text });
+  };
+
+  // 특정 투두 삭제 액션을 호출하는 함수
+  const deleteTodo = id => {
+    dispatch({ type: 'DELETE_TODO', payload: id });
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} deleteTodo={deleteTodo} />
+    </div>
+  );
+};
+
+export default App;
